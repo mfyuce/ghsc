107ce5528ec68a32780a09b28c9577c90e328093
myuce <mehmet.yuce@organonanalytics.com>
2016-06-09 15:17:41 +0300

    finished tw routing

---
+++ src/main/scala/com/ghsc/SolomonOptions.scala
@@ -0,0 +1,53 @@
+package com.ghsc.solomon
+
+import com.ghsc.GeneralOptions
+
+
+object R1Options  extends GeneralOptions{
+  TXT_INSTANCE_FOLDER = "data/solomon-100/R1/"
+  numberOfLocationCenters =1
+  numberOfTWCenters =1
+  numberOfRoutingRun = 5000
+  numberOfResultingRoutingRun = 256
+  numberOfIterations = 5000
+}
+object R2Options  extends GeneralOptions{
+  TXT_INSTANCE_FOLDER = "data/solomon-100/R2/"
+  numberOfLocationCenters =1
+  numberOfTWCenters =1
+  numberOfRoutingRun =  5000
+  numberOfResultingRoutingRun = 1
+  numberOfIterations = 5000
+}
+object C1Options  extends GeneralOptions{
+  TXT_INSTANCE_FOLDER = "data/solomon-100/C1/"
+  numberOfLocationCenters =1
+  numberOfTWCenters =1
+  numberOfRoutingRun =  5000
+  numberOfResultingRoutingRun = 1
+  numberOfIterations = 5000
+}
+object C2Options  extends GeneralOptions{
+  TXT_INSTANCE_FOLDER = "data/solomon-100/C2/"
+  numberOfLocationCenters =1
+  numberOfTWCenters =1
+  numberOfRoutingRun =  5000
+  numberOfResultingRoutingRun = 1
+  numberOfIterations = 5000
+}
+object RC1Options  extends GeneralOptions{
+  TXT_INSTANCE_FOLDER = "data/solomon-100/RC1/"
+  numberOfLocationCenters =1
+  numberOfTWCenters =1
+  numberOfRoutingRun =  5000
+  numberOfResultingRoutingRun = 1
+  numberOfIterations = 5000
+}
+object RC2Options  extends GeneralOptions{
+  TXT_INSTANCE_FOLDER = "data/solomon-100/RC2/"
+  numberOfLocationCenters =1
+  numberOfTWCenters =1
+  numberOfRoutingRun =  5000
+  numberOfResultingRoutingRun = 1
+  numberOfIterations = 5000
+}
